162d4ece92e2c60689c3192a6bb8a448fefae746
Gustavo Leck <gustavoleck@hotmail.com>
2023-04-26 20:15:58 -0300

    fix(css alterado)

--- src/components/CampoBusca/styles.jsx
+++ src/components/CampoBusca/styles.jsx
@@ -3,9 +3,10 @@ import styled from "styled-components";
 export const Container = styled.label`
   display: flex;
   align-items: center;
-  margin-right: 5px;
+  margin-left 5%;
 `;
 
 export const Campo = styled.input`
   border-radius: 10px;
+  margin-left: 2%;
 `;
