d48b6d6bb39119ab78d157396b98f1e2ff16f165
Gustavo Leck <gustavoleck@hotmail.com>
2023-04-25 23:04:31 -0300

    feat(Adicionado funcionabilidade de formulario de pesquisa)

--- src/components/CampoBusca/styles.jsx
+++ src/components/CampoBusca/styles.jsx
@@ -1,20 +1,11 @@
 import styled from "styled-components";
 
-export const Campo = styled.input`
-  border-radius: 5px;
-  margin-left: 2%;
-  width: 66%;
-  box-shadow: 0px 0px 8px 2px #8e8777;
-`;
-
-export const Container = styled.div`
+export const Container = styled.label`
   display: flex;
   align-items: center;
-  // border: 3px solid blue;
-  width: 30%;
-  padding-left: 1%;
+  margin-right: 5px;
 `;
 
-export const Texto = styled.p`
-  text-align: right;
+export const Campo = styled.input`
+  border-radius: 10px;
 `;
