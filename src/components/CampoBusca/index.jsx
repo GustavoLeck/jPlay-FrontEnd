7b1801ccfd61fe322040d1e7d4260b43824054bd
Gustavo Leck <gustavoleck@hotmail.com>
2023-04-26 19:09:09 -0300

    fix(2 pontos campo de busca)

--- src/components/CampoBusca/index.jsx
+++ src/components/CampoBusca/index.jsx
@@ -3,7 +3,7 @@ import { Campo, Container } from "./styles";
 function CampoBusca({ Nome }) {
   return (
     <Container>
-      {Nome} : <Campo name={Nome}></Campo>
+      {Nome + ":"} <Campo name={Nome}></Campo>
     </Container>
   );
 }
